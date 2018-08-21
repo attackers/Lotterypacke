54f9b64384697f4f3b1a592bacacadc5f75838e1
OS X10_12_6 <lf@mifengs.com>
2018-08-21 09:32:42 +0800

    first commit

---
+++ Lotterypacke/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lotterypacke
+//
+//  Created by OS X10_12_6 on 2018/8/21.
+//  Copyright © 2018年 OS X10_12_6. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lotterypacke/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lotterypacke
+//
+//  Created by OS X10_12_6 on 2018/8/21.
+//  Copyright © 2018年 OS X10_12_6. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
